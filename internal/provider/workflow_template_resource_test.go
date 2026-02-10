c8a677bdab1fd1b455b9ead527c5f84c064becde
Aditya Choudhari <adityachoudhari26@gmail.com>
2026-02-10 15:14:45 -0800

    feat: workflow template provider

---
+++ internal/provider/workflow_template_resource_test.go
@@ -0,0 +1,100 @@
+// Copyright (c) HashiCorp, Inc.
+
+package provider
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/knownvalue"
+	"github.com/hashicorp/terraform-plugin-testing/statecheck"
+	"github.com/hashicorp/terraform-plugin-testing/tfjsonpath"
+)
+
+func TestAccWorkflowTemplateResource(t *testing.T) {
+	name := fmt.Sprintf("tf-acc-wft-%d", time.Now().UnixNano())
+	updatedName := name + "-updated"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and verify
+			{
+				Config: testAccWorkflowTemplateResourceConfig(name, "default-val", 5, "successful"),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"ctrlplane_workflow_template.test",
+						tfjsonpath.New("id"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"ctrlplane_workflow_template.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(name),
+					),
+					statecheck.ExpectKnownValue(
+						"ctrlplane_workflow_template.test",
+						tfjsonpath.New("input"),
+						knownvalue.ListSizeExact(1),
+					),
+					statecheck.ExpectKnownValue(
+						"ctrlplane_workflow_template.test",
+						tfjsonpath.New("job"),
+						knownvalue.ListSizeExact(1),
+					),
+				},
+			},
+			// Update and verify
+			{
+				Config: testAccWorkflowTemplateResourceConfig(updatedName, "updated-val", 10, "failure"),
+				ConfigStateChecks: []statecheck.StateCheck{
+					statecheck.ExpectKnownValue(
+						"ctrlplane_workflow_template.test",
+						tfjsonpath.New("id"),
+						knownvalue.NotNull(),
+					),
+					statecheck.ExpectKnownValue(
+						"ctrlplane_workflow_template.test",
+						tfjsonpath.New("name"),
+						knownvalue.StringExact(updatedName),
+					),
+				},
+			},
+		},
+	})
+}
+
+func testAccWorkflowTemplateResourceConfig(name string, defaultInput string, delaySeconds int, status string) string {
+	return fmt.Sprintf(`
+%s
+resource "ctrlplane_job_agent" "test" {
+  name = %q
+
+  test_runner {
+    delay_seconds = %d
+    status        = %q
+  }
+}
+
+resource "ctrlplane_workflow_template" "test" {
+  name = %q
+
+  input {
+    name           = "env"
+    type           = "string"
+    default_string = %q
+  }
+
+  job {
+    name = "deploy"
+    ref  = ctrlplane_job_agent.test.id
+    config = {
+      image = "deploy:latest"
+    }
+  }
+}
+`, testAccProviderConfig(), name+"-ja", delaySeconds, status, name, defaultInput)
+}
